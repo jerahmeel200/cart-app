c304736638383568bc871af27001b3818da63bcd
DESKTOP-14E8GN0\JAMICO <jamicojerahmeel@gmail.com>
2024-07-10 19:45:31 +0100

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -30,7 +30,7 @@ const articles = [
   },
   {
     image: image5,
-    desc: 'AirPods Max',
+    desc: 'AirPods Max | Apple M3 Max | 1TB',
     price: '₦1,099,000.00',
   },
   {
@@ -87,7 +87,7 @@ const Card = () => {
     {articles.map((article, index) => (
       <div 
         key={index} 
-        className="bg-white p-4 py-2 rounded-lg shadow-md w-[157px] h-[167px] sm:w-[300px] sm:h-[400px] md:w-[430px] md:h-[490px] mx-auto flex flex-col justify-between"
+        className="bg-white p-4 py-1 rounded-lg shadow-md w-[157px] h-[167px] sm:w-[300px] sm:h-[400px] md:w-[430px] md:h-[490px] mx-auto flex flex-col justify-between"
       >
         <div>
           <Image 
@@ -95,8 +95,11 @@ const Card = () => {
             alt={article.desc} 
             className="mx-auto mb-2 w-[87.47px] h-[88.31px] sm:w-[190px] sm:h-[200px] md:w-[227.33px] md:h-[269px] object-contain" 
           />
-          <h2 className="text-[7.22px] leading-[11.88px] sm:text-[22px] sm:leading-[36.18px] md:text-[22px] md:leading-[36.18px] font-semibold ">{article.desc}</h2>
+          <div>
+          <h2 className="text-[7.22px] leading-[11.88px] sm:text-[22px] sm:leading-[36.18px] md:text-[20px] md:leading-[36.18px] font-semibold ">{article.desc}</h2>
           <p className="text-[7.22px] leading-[11.88px] sm:text-[22px] sm:leading-[36.18px] md:text-[22px] md:leading-[36.18px] font-semibold ">{article.price}</p>
+          </div>
+        
         </div>
         <div className="flex gap-2">
           <button className="bg-[#145771] text-white text-[7.88px] leading-[12.96px] sm:text-[19px] sm:leading-[30px] md:text-[24px] md:leading-[39.47px] w-[62.42px] h-[17.4px] sm:w-[150px] sm:h-[40px] md:w-[162.23px] md:h-[53px] rounded">Buy Now</button>
